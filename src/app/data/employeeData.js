ed87477578680db1cbf7b445ebb1cfb0c679d2df
titusedwardgoh <titus.edward.goh@gmail.com>
2025-06-16 06:15:07 +1000

    updated titles and names

--- src/app/data/employeeData.js
+++ src/app/data/employeeData.js
@@ -1,62 +1,62 @@
 const employeeData = [
   {
-    name: "Emily Thompson",      // f
+    name: "Mira Bass",      // f
     title: "Founder & CEO",
     src: { url: "/employees_01.jpg", width: 362, height: 353 },
   },
   {
     name: "Olivia Martinez",     // f
-    title: "Founder & CEO",
+    title: "Director of Marketing Services",
     src: { url: "/employees_02.jpg", width: 362, height: 353 },
   },
   {
-    name: "Michael Johnson",     // m
-    title: "Founder & CEO",
+    name: "Otis Bull",     // m
+    title: "Director of Digital Marketing",
     src: { url: "/employees_03.jpg", width: 362, height: 353 },
   },
   {
-    name: "David Wilson",        // m
-    title: "Founder & CEO",
+    name: "Harvey Beltran",        // m
+    title: "Brand Manager",
     src: { url: "/employees_04.jpg", width: 362, height: 353 },
   },
   {
-    name: "James Brown",         // m
-    title: "Founder & CEO",
+    name: "Omer Mays",         // m
+    title: "Digital Content Manager",
     src: { url: "/employees_05.jpg", width: 362, height: 353 },
   },
   {
-    name: "Sophia Davis",        // f
-    title: "Founder & CEO",
+    name: "Ella Weber",        // f
+    title: "SEO Manager",
     src: { url: "/employees_06.jpg", width: 362, height: 353 },
   },
   {
-    name: "William Miller",      // m
-    title: "Founder & CEO",
+    name: "Connor Dennis",      // m
+    title: "Account Executive",
     src: { url: "/employees_07.jpg", width: 362, height: 353 },
   },
   {
-    name: "Isabella Anderson",   // f
-    title: "Founder & CEO",
+    name: "Aalia Cuevas",   // f
+    title: "Account Executive",
     src: { url: "/employees_08.jpg", width: 362, height: 353 },
   },
   {
-    name: "Mia Martinez",        // f
-    title: "Founder & CEO",
+    name: "Taryn Cairns",        // f
+    title: "Account Executive",
     src: { url: "/employees_09.jpg", width: 362, height: 353 },
   },
   {
-    name: "Benjamin Taylor",     // m
-    title: "Founder & CEO",
+    name: "Coby Bevan",     // m
+    title: "Social Media Manager",
     src: { url: "/employees_10.jpg", width: 362, height: 353 },
   },
   {
-    name: "Charlotte Thomas",    // f
-    title: "Founder & CEO",
+    name: "Cloe Abbott",    // f
+    title: "PPC Manager",
     src: { url: "/employees_11.jpg", width: 362, height: 353 },
   },
   {
-    name: "Alexander Moore",     // m
-    title: "Founder & CEO",
+    name: "Julius Avalos",     // m
+    title: "PPC Manager",
     src: { url: "/employees_12.jpg", width: 362, height: 353 },
   },
 ];
